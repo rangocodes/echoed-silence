457ff44c0c92d47325efdf228573e2b421a96df4
rangocodes <arnoldcliff99@gmail.com>
2024-07-23 18:42:58 +0300

    Firebase API key masking
    Signed-off-by: rangocodes <arnoldcliff99@gmail.com>

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,8 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import {initializeApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
@@ -13,4 +13,4 @@ console.log(firebaseConfig)
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db };
+export {db};
